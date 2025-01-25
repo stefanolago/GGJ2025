dbdd559a070228e6db5a99e1065410edc9ade546
Merlin <liberomasini91@gmail.com>
2025-01-26 00:09:45 +0100

    animation for moving bubbles

--- godot/game/entities/bubble/bubble_states/popped_state.gd
+++ godot/game/entities/bubble/bubble_states/popped_state.gd
@@ -4,7 +4,7 @@ extends BubbleState
 func enter() -> void:
 	bubble.sprite.set_body_status("popped")
 	bubble.pop()
-	bubble.body_anim_player.play("Idle")
+	bubble.body_anim_player.play("Idle_0")
 
 
 func exit() -> void:
