b621b65177162b56bf7f1a6c6bf1e13893c539d9
stefanolago <jafarrolo@gmail.com>
2025-01-26 10:32:28 +0100

    Fixed credits

--- godot/scripts/ending.gd
+++ godot/scripts/ending.gd
@@ -1,14 +1,18 @@
 extends CanvasLayer
 
 @export var good_ending: bool = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	($AnimationPlayer as AnimationPlayer).play("show_credits")
+	
 	if good_ending:
 		await get_tree().create_timer(60).timeout
 		_end_game()
 	else:
 		Dialogic.timeline_ended.connect(_end_game)
 		Dialogic.start("ending")
+	
 
 func _end_game() -> void:
 	get_tree().quit()
