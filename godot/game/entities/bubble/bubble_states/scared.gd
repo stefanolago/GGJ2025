f0c4ca0b8da92807a467c2ca65d2879c8f1535cd
stefanolago <jafarrolo@gmail.com>
2025-01-26 09:04:24 +0100

    Updates to bubble scared

--- godot/game/entities/bubble/bubble_states/scared.gd
+++ godot/game/entities/bubble/bubble_states/scared.gd
@@ -7,5 +7,19 @@ func enter() -> void:
 	# start an animation to detach itself from the bubblewrap?
 	# have to insert a delay since the previous state needs
 	# to finish disconnecting its signals
-	await get_tree().create_timer(0.2).timeout
+	# create a tween that makes the bubble wobble
+	# by changing the scale horizontally and vertically
+	var tween: Tween = get_tree().create_tween()
+
+	tween.tween_property(bubble, "scale", Vector2(1.1, 0.9), 0.1)
+	tween.tween_property(bubble, "scale", Vector2(1.0, 1.0), 0.1)
+	tween.tween_property(bubble, "scale", Vector2(0.9, 1.1), 0.1)
+	tween.tween_property(bubble, "scale", Vector2(1.0, 1.0), 0.1)
+	tween.tween_property(bubble, "scale", Vector2(1.1, 0.9), 0.1)
+	tween.tween_property(bubble, "scale", Vector2(1.0, 1.0), 0.1)
+	tween.tween_property(bubble, "scale", Vector2(0.9, 1.1), 0.1)
+	tween.tween_property(bubble, "scale", Vector2(1.0, 1.0), 0.1)
+	await tween.finished
+
+
 	transition.emit("RunningAway")
